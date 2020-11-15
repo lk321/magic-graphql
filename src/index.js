7fdbf5aee58bc7f182cda724e7433e1d0e77dccf
lk321 <orozco6453@gmail.com>
2020-11-14 23:25:52 -0800

    micro function

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const defaultOptions = {
  * 
  * @returns {ApolloServer} Returns object of ApolloServer
  */
-module.exports = (app, options = defaultOptions) => {
+const Module = module.exports = (app, options = defaultOptions) => {
     options = Object.assign({}, defaultOptions, options)
 
     if (!app) throw new Error("app is required")
@@ -57,20 +57,64 @@ module.exports = (app, options = defaultOptions) => {
     }
 
     let context = null
-    if(options.contextWrapper && typeof options.contextWrapper ==='function'){
-        context = options.contextWrapper( options.context )
+    if (options.contextWrapper && typeof options.contextWrapper === 'function') {
+        context = options.contextWrapper(options.context)
     }
-    else{ context = options.context }
+    else { context = options.context }
 
     const graphqlServer = new ApolloServer({
         schema: new GraphQLSchema(schemas),
         context,
         tracing: options.tracing || false
     })
-    
+
     app.use(compression({ level: 1 }))
     graphqlServer.applyMiddleware({ app, path: options.graphqlEndpint, compression: compression() })
     if (options.subscriptions) graphqlServer.installSubscriptionHandlers(options.httpServer)
 
     return graphqlServer
+}
+
+Module.micro = (options = defaultOptions) => {
+    options = Object.assign({}, defaultOptions, options)
+
+    if (!options.modelDirPath || (typeof options.modelDirPath !== 'string' && typeof options.modelDirPath !== 'object')) throw new Error("options.modelDirPath has an incorrect value, this option is required")
+    if (options.dataloader && !options.dataloaderOptions) throw new Error("dataloaderOptions can't be null")
+    if (options.customsDirPath && typeof options.customsDirPath !== 'string') throw new Error("options.customsDirPath has an incorrect value")
+
+    let models = null
+    if (typeof options.modelDirPath === 'string') models = require(options.modelDirPath)
+    else models = options.modelDirPath
+
+    let schemas = generateSchema(models, null, options)
+
+    if (options.dataloader) {
+        if (options.context && typeof options.context === 'object') {
+            options.context = Object.assign({}, options.context, {
+                dataloaderContext: createContext(models.sequelize, options.dataloaderOptions),
+                models
+            })
+        } else if (options.context && typeof options.context === 'function') {
+            options.context = (integrationContext) => {
+                const optionsContext = options.context(integrationContext)
+                return Object.assign({}, optionsContext, {
+                    dataloaderContext: createContext(models.sequelize, options.dataloaderOptions),
+                    models
+                })
+            }
+        }
+    }
+
+    let context = null
+    if (options.contextWrapper && typeof options.contextWrapper === 'function') {
+        context = options.contextWrapper(options.context)
+    }
+    else { context = options.context }
+
+    return ({
+        schema: new GraphQLSchema(schemas),
+        context,
+        tracing: options.tracing || false
+    })
+
 }
